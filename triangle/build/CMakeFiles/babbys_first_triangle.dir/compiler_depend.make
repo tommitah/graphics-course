49e7e8e056db69697c5669d00d82329dc5535a3b
tommitah <reijotahvanainen15@gmail.com>
2022-11-29 21:38:35 +0200

    initial commit, babbys first triangle

---
+++ triangle/build/CMakeFiles/babbys_first_triangle.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for babbys_first_triangle.
+# This may be replaced when dependencies are built.
---
+++ triangle/build/CMakeFiles/babbys_first_triangle.dir/main.cpp.o.d
@@ -0,0 +1,14 @@
+CMakeFiles/babbys_first_triangle.dir/main.cpp.o: \
+ /home/tommitah/development/graphics-course/triangle/main.cpp \
+ /usr/include/stdc-predef.h /usr/include/glad/glad.h \
+ /usr/include/glad/khrplatform.h \
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/10.2/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/timesize.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/wchar.h /usr/include/bits/stdint-intn.h \
+ /usr/include/bits/stdint-uintn.h /usr/include/GLFW/glfw3.h \
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/10.2/include/stddef.h
