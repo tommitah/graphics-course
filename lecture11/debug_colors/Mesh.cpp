19145d7551fd5fc8d31b9a05cbeb61c41484621d
tommitah <reijotahvanainen15@gmail.com>
2022-12-19 12:56:39 +0200

    lecture11: mesh class setup

---
+++ lecture11/debug_colors/Mesh.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <glad/glad.h>
+#include "objloader.hpp"
+#include "Mesh.h"
+
+Mesh::Mesh(std::string file, GLuint shader) {}
+Mesh::~Mesh() {}
+void Mesh::Clear() {}
+void Mesh::DrawMesh() {}
+void Mesh::Translate(glm::vec3) {}
+void Mesh::Load() {}
---
+++ lecture11/debug_colors/objloader.hpp
@@ -0,0 +1,25 @@
+#ifndef OBJLOADER_H
+#define OBJLOADER_H
+#include <iostream>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+bool loadOBJ(
+	const char * path, 
+	std::vector<glm::vec3> & out_vertices, 
+	std::vector<glm::vec2> & out_uvs, 
+	std::vector<glm::vec3> & out_normals
+);
+
+
+
+bool loadAssImp(
+	const char * path, 
+	std::vector<unsigned short> & indices,
+	std::vector<glm::vec3> & vertices,
+	std::vector<glm::vec2> & uvs,
+	std::vector<glm::vec3> & normals
+);
+
+#endif
