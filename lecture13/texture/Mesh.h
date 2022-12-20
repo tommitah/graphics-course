c3a917b6d2daf24ff986d8affd9eb9d1cf1dac96
tommitah <reijotahvanainen15@gmail.com>
2022-12-20 13:16:59 +0200

    lec13: texture setup

---
+++ lecture13/texture/Mesh.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+#include "Transform.h"
+
+class Mesh : public Transform {
+public:
+	std::string m_file;
+	Mesh();
+	~Mesh();
+	void Init(std::string file, GLuint shader);
+	void Clear();
+	void DrawMesh();
+
+private:
+	std::vector<glm::vec3> m_vertices;
+	std::vector<glm::vec2> m_uvs;
+	std::vector<glm::vec3> m_normals;
+	GLuint m_vbo;
+	GLuint m_vuv;
+	GLuint m_vno;
+	GLuint m_vao;
+	GLuint m_modelMatrixID;
+	GLuint m_itMatrixID;
+	GLuint m_shader;
+
+	void Load();
+};
+
